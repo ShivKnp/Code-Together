7a2c17d0f3ab98f513af9a16e521143b41f86f7c
Shivansh Tiwari <113789714+Fearman99@users.noreply.github.com>
2024-08-14 22:13:04 +0530

    Update Actions.js

--- client/src/Actions.js
+++ client/src/Actions.js
@@ -1,24 +1,10 @@
-// const ACTIONS = {
-//     JOIN: 'join',
-//     JOINED: 'joined',
-//     DISCONNECTED: 'disconnected',
-//     CODE_CHANGE: 'code-change',
-//     SYNC_CODE: 'sync-code',
-//     LEAVE: 'leave',
-// };
-
-// module.exports = ACTIONS;
-
 const ACTIONS = {
     JOIN: 'join',
-    JOIN: 'join',
-  JOINED: 'joined',
-  CODE_CHANGE: 'code_change',
-  SYNC_CODE: 'sync_code',
-  COMPILE_CPP: 'compile_cpp',
-  COMPILATION_RESULT: 'compilation_result',
-  DISCONNECTED: 'disconnected',
+    JOINED: 'joined',
+    DISCONNECTED: 'disconnected',
+    CODE_CHANGE: 'code-change',
+    SYNC_CODE: 'sync-code',
+    LEAVE: 'leave',
 };
 
 module.exports = ACTIONS;
-
